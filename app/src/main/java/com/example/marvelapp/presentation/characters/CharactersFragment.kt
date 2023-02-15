856e5c10338bac16986b546d48e8b96f8c13587e
Gui55 <gui.mergulhao55@outlook.com>
2023-02-15 14:20:53 -0300

    Exibição de personagens

--- app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.marvelapp.R
 import com.example.marvelapp.databinding.FragmentCharactersBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
 @AndroidEntryPoint
 class CharactersFragment : Fragment() {
 
@@ -15,6 +20,8 @@ class CharactersFragment : Fragment() {
     private val binding : FragmentCharactersBinding get() = _binding!!
 
     private val charactersAdapter = CharactersAdapter()
+
+    private val viewModel: CharactersViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,6 +36,12 @@ class CharactersFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initCharactersAdapter()
+
+        lifecycleScope.launch{
+            viewModel.charactersPagingData("").collect{ pagingData ->
+                charactersAdapter.submitData(pagingData)
+            }
+        }
     }
 
     private fun initCharactersAdapter(){
